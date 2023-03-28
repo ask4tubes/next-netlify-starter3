e6b7c63f64d5ae5575b731bfb4c06ec9609c4f34
ask4tubes <104836892+ask4tubes@users.noreply.github.com>
2023-03-28 09:22:27 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,32 @@ export default function Home() {
       <main>
         <Header title="Welcome to Tubes Lab." />
         <p className="description">
-          
+    
+        <hr align="center" width="20%">
+    <!DOCTYPE html>
+<html>
+<head>
+<title>
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {background-color:#ffffff;background-repeat:no-repeat;background-position:top left;background-attachment:fixed;}
+h1{font-family:Arial, sans-serif;color:#000000;background-color:#ffffff;}
+p {text-align:center;font-family:Helvetica, sans-serif;font-size:16px;font-style:normal;font-weight:normal;color:#000000;background-color:#ffffff;}
+</style>
+</head>
+<body>
+<h1></h1>
+<p>___________________</p>
+<p>restoration</p>
+<p></p>
+<p>modification</p>
+<p></p>
+<p>custom design</p>
+</body>
+</html>
+
+        <center>restoration</center>
         </p>
       </main>
 
